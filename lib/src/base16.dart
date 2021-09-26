473dcb435f7bd41178ebd0bfff779e35cb2777f0
Arthur Kirsanov <kirsappsinc@gmail.com>
2021-09-26 19:45:21 +0300

    added base16 documentation

--- lib/src/base16.dart
+++ lib/src/base16.dart
@@ -1,18 +1,39 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+/// A [base16](https://tools.ietf.org/html/rfc4648) encoder and decoder.
+///
+/// It encodes and decodes using the base16 alphabet.
+/// It does not allow invalid characters when decoding.
 const Base16Codec base16 = Base16Codec();
 
+/// Encodes [input] using [base16](https://tools.ietf.org/html/rfc4648) encoding.
+///
+/// Shorthand for `base16.encode(input)`.
+/// Useful if a local variable shadows the global [base16] constant.
 String base16Encode(Uint8List input) => base16.encode(input);
 
+/// Decodes [input] using [base16](https://tools.ietf.org/html/rfc4648) decoding.
+///
+/// Shorthand for `base16.decode(input)`.
+/// Useful if a local variable shadows the global [base16] constant.
 Uint8List base16Decode(String input) => base16.decode(input);
 
+/// Hex (base16) codec alias to [base16]
 const hex = base16;
 
+/// Encodes [input] using [base16](https://tools.ietf.org/html/rfc4648)
+/// alias to [base16Encode]
 String hexEncode(Uint8List input) => base16Encode(input);
 
+/// Decodes [input] using [base16](https://tools.ietf.org/html/rfc4648)
+/// alias to [base16Decode]
 Uint8List hexDecode(String input) => base16Decode(input);
 
+/// A [base16](https://tools.ietf.org/html/rfc4648) encoder and decoder.
+///
+/// It encodes and decodes using the base16 alphabet.
+/// It does not allow invalid characters when decoding.
 class Base16Codec extends Codec<Uint8List, String> {
   static const String _alphabet = "0123456789ABCDEF";
   const Base16Codec();
@@ -24,6 +45,9 @@ class Base16Codec extends Codec<Uint8List, String> {
   Converter<String, Uint8List> get decoder => const Base16Decoder(_alphabet);
 }
 
+/// A Base16 encoder and decoder with custom [alphabet].
+///
+/// It does not allow invalid characters when decoding.
 class Base16CodecCustom extends Codec<Uint8List, String> {
   final String alphabet;
   const Base16CodecCustom(this.alphabet);
@@ -35,6 +59,11 @@ class Base16CodecCustom extends Codec<Uint8List, String> {
   Converter<String, Uint8List> get decoder => Base16Decoder(alphabet);
 }
 
+/// Base16 encoding converter with given [_alphabet].
+///
+/// Encodes lists of bytes using Rfc [base16] alphabet and [Base16CodecCustom] alphabet.
+///
+/// The results are ASCII strings using a restricted alphabet.
 class Base16Encoder extends Converter<Uint8List, String> {
   final String _alphabet;
   const Base16Encoder(this._alphabet);
@@ -52,6 +81,7 @@ class Base16Encoder extends Converter<Uint8List, String> {
   }
 }
 
+/// Decoder for base16 encoded data based on given [_alphabet].
 class Base16Decoder extends Converter<String, Uint8List> {
   final String _alphabet;
   const Base16Decoder(this._alphabet);

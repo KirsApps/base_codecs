8da31ca6e24350560a16f39302fe99df2af88aec
Arthur Kirsanov <kirsappsinc@gmail.com>
2021-09-27 23:09:18 +0300

    tests for coverage

--- test/base_codecs_test.dart
+++ test/base_codecs_test.dart
@@ -11,10 +11,10 @@ void main() {
   group('base16', () {
     const data = [0xDE, 0xAD, 0xBE, 0xEF];
     test('encode', () {
-      expect(base16.encode(Uint8List.fromList(data)), equals('DEADBEEF'));
+      expect(hexEncode(Uint8List.fromList(data)), equals('DEADBEEF'));
     });
     test('decode', () {
-      expect(base16.decode('DEADBEEF'), data);
+      expect(hexDecode('DEADBEEF'), data);
     });
     const custom = Base16CodecCustom('ABCDEF9876543210');
     final customData = Uint8List.fromList(
@@ -37,90 +37,85 @@ void main() {
           "JVQW4IDJOMQGI2LTORUW4Z3VNFZWQZLEFQQG433UEBXW43DZEBRHSIDINFZSA4TFMFZW63RMEBRHK5BAMJ4SA5DINFZSA43JNZTXK3DBOIQHAYLTONUW63RAMZZG63JAN52GQZLSEBQW42LNMFWHGLBAO5UGSY3IEBUXGIDBEBWHK43UEBXWMIDUNBSSA3LJNZSCYIDUNBQXIIDCPEQGCIDQMVZHGZLWMVZGC3TDMUQG6ZRAMRSWY2LHNB2CA2LOEB2GQZJAMNXW45DJNZ2WKZBAMFXGIIDJNZSGKZTBORUWOYLCNRSSAZ3FNZSXEYLUNFXW4IDPMYQGW3TPO5WGKZDHMUWCAZLYMNSWKZDTEB2GQZJAONUG64TUEB3GK2DFNVSW4Y3FEBXWMIDBNZ4SAY3BOJXGC3BAOBWGKYLTOVZGKLQ=";
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base32RfcEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base32RfcDecode(encoded), utf8.encode(testString));
       });
     });
     group('HEX', () {
-      const codec = Base32CodecRfcHex();
       const encoded =
           "9LGMS839ECG68QBJEHKMSPRLD5PMGPB45GG6SRRK41NMSR3P41H7I838D5PI0SJ5C5PMURHC41H7AT10C9SI0T38D5PI0SR9DPJNAR31E8G70OBJEDKMURH0CPP6UR90DTQ6GPBI41GMSQBDC5M76B10ETK6IOR841KN683141M7ASRK41NMC83KD1II0RB9DPI2O83KD1GN8832F4G6283GCLP76PBMCLP62RJ3CKG6UPH0CHIMOQB7D1Q20QBE41Q6GP90CDNMST39DPQMAP10C5N68839DPI6APJ1EHKMEOB2DHII0PR5DPIN4OBKD5NMS83FCOG6MRJFETM6AP37CKM20PBOCDIMAP3J41Q6GP90EDK6USJK41R6AQ35DLIMSOR541NMC831DPSI0OR1E9N62R10E1M6AOBJELP6ABG=";
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base32RfcHexEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base32RfcHexDecode(encoded), utf8.encode(testString));
       });
     });
     group('ZBase', () {
-      const codec = Base32CodecZBase();
       const encoded =
           "JIOSHEDJQCOGE4MUQTWSH35IPF3SO3MRFOOGH55WRBZSH5D3RBT81EDEPF31YHUFCF3S65TCRBT8K7BYCJH1Y7DEPF31YH5JP3UZK5DBQEO8YAMUQPWS65TYC33G65JYP74GO3M1RBOSH4MPCFS8GMBYQ7WG1A5ERBWZGEDBRBS8KH5WRBZSCEDWPB11Y5MJP31NAEDWPBOZEEDNXROGNEDOCI38G3MSCI3GN5UDCWOG63TYCT1SA4M8PB4NY4MQRB4GO3JYCPZSH7DJP34SK3BYCFZGEEDJP31GK3UBQTWSQAMNPT11Y35FP31ZRAMWPFZSHEDXCAOGS5UXQ7SGK3D8CWSNY3MACP1SK3DURB4GO3JYQPWG6HUWRB5GK4DFPI1SHA5FRBZSCEDBP3H1YA5BQJZGN5BYQBSGKAMUQI3GKMO";
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base32ZBaseEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base32ZBaseDecode(encoded), utf8.encode(testString));
       });
     });
     group('Crockford', () {
-      const codec = Base32CodecCrockford();
       const encoded =
           "9NGPW839ECG68TBKEHMPWSVND5SPGSB45GG6WVVM41QPWV3S41H7J838D5SJ0WK5C5SPYVHC41H7AX10C9WJ0X38D5SJ0WV9DSKQAV31E8G70RBKEDMPYVH0CSS6YV90DXT6GSBJ41GPWTBDC5P76B10EXM6JRV841MQ683141P7AWVM41QPC83MD1JJ0VB9DSJ2R83MD1GQ8832F4G6283GCNS76SBPCNS62VK3CMG6YSH0CHJPRTB7D1T20TBE41T6GS90CDQPWX39DSTPAS10C5Q68839DSJ6ASK1EHMPERB2DHJJ0SV5DSJQ4RBMD5QPW83FCRG6PVKFEXP6AS37CMP20SBRCDJPAS3K41T6GS90EDM6YWKM41V6AT35DNJPWRV541QPC831DSWJ0RV1E9Q62V10E1P6ARBKENS6ABG";
       const encodedWithReplaceSymbols =
           "9NGPW839ECG68TBKEHMPWSVND5SPGSB45GG6WVVM41QPWV3S41H7J838D5SJoWK5C5SPYVHC41H7AX10C9WJOX38D5SJOWV9DSKQAV31E8G70RBKEDMPYVH0CSS6YV90DXT6GSBJ41GPWTBDC5P76B10EXM6JRV84lMQ683i4IP7AWVM41QPC83MDiJJ0VB9DSJ2R83MD1GQ8832F4G6283GCNS76SBPCNS62VK3CMG6YSH0CHJPRTB7D1T20TBE4IT6GS90CDQPWX39DSTPAS10C5Q68839DSJ6ASKLEHMPERB2DHJJ0SV5DSJQ4RBMD5QPW83FCRG6PVKFEXP6AS37CMP2oSBRCDJPAS3K41T6GS90EDM6YWKM4LV6AT35DNJPWRV541QPC831DSWJ0RVIE9Q62V10E1P6ARBKENS6ABG";
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base32CrockfordEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode with i I l L  o O symbols', () {
         expect(
-          codec.decode(encodedWithReplaceSymbols),
+          base32CrockfordDecode(encodedWithReplaceSymbols),
           utf8.encode(testString),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base32CrockfordDecode(encoded), utf8.encode(testString));
       });
     });
     group('WordSafe', () {
-      const codec = Base32CodecWordSafe();
       const encoded =
           "FfRgrC5FPJR8CpHXPVcgrmqfM7mgRmH67RR8rqqc63hgrq5m63V9WC5CM7mW2rX7J7mgwqVJ63V9Gv32JFrW2v5CM7mW2rqFMmXhGq53PCR92jHXPMcgwqV2Jmm8wqF2Mvp8RmHW63RgrpHMJ7g98H32Pvc8WjqC63ch8C5363g9Grqc63hgJC5cM3WW2qHFMmW4jC5cM3RhCC54Q6R84C5RJfm98mHgJfm84qX5JcR8wmV2JVWgjpH9M3p42pHP63p8RmF2JMhgrv5FMmpgGm32J7h8CC5FMmW8GmX3PVcgPjH4MVWW2mq7MmWh6jHcM7hgrC5QJjR8gqXQPvg8Gm59Jcg42mHjJMWgGm5X63p8RmF2PMc8wrXc63q8Gp57MfWgrjq763hgJC53MmrW2jq3PFh84q32P3g8GjHXPfm8GHR";
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base32WordSafeEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base32WordSafeDecode(encoded), utf8.encode(testString));
       });
     });
     group('GeoHash', () {
-      const codec = Base32CodecGeoHash();
       const encoded =
           "9phqw839fdh68ucmfjnqwtvpe5tqhtc45hh6wvvn41rqwv3t41j7k838e5tk0wm5d5tqyvjd41j7bx10d9wk0x38e5tk0wv9etmrbv31f8h70scmfenqyvj0dtt6yv90exu6htck41hqwuced5q76c10fxn6ksv841nr683141q7bwvn41rqd83ne1kk0vc9etk2s83ne1hr8832g4h6283hdpt76tcqdpt62vm3dnh6ytj0djkqsuc7e1u20ucf41u6ht90derqwx39etuqbt10d5r68839etk6btm1fjnqfsc2ejkk0tv5etkr4scne5rqw83gdsh6qvmgfxq6bt37dnq20tcsdekqbt3m41u6ht90fen6ywmn41v6bu35epkqwsv541rqd831etwk0sv1f9r62v10f1q6bscmfpt6bch";
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base32GeoHashEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base32GeoHashDecode(encoded), utf8.encode(testString));
       });
     });
     group('Custom', () {
@@ -180,7 +175,7 @@ void main() {
           test('encode', () {
             for (final set in testData) {
               expect(
-                codec.encode(Uint8List.fromList(base16.decode(set[0]))),
+                base58BitcoinEncode(Uint8List.fromList(base16.decode(set[0]))),
                 equals(set[1]),
               );
             }
@@ -188,7 +183,7 @@ void main() {
           test('decode', () {
             for (final set in testData) {
               expect(
-                base16.encode(codec.decode(set[1])),
+                base16.encode(base58BitcoinDecode(set[1])),
                 equals(set[0].toUpperCase()),
               );
             }
@@ -197,45 +192,44 @@ void main() {
       );
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base58BitcoinEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base58BitcoinDecode(encoded), utf8.encode(testString));
       });
       test('encode with zero', () {
         expect(
-          codec.encode(Uint8List.fromList(dataWithZero)),
+          base58BitcoinEncode(Uint8List.fromList(dataWithZero)),
           equals(encodedDataWithZero),
         );
       });
       test('decode with zero', () {
-        expect(codec.decode(encodedDataWithZero), dataWithZero);
+        expect(base58BitcoinDecode(encodedDataWithZero), dataWithZero);
       });
     });
     group('Flickr', () {
-      const codec = Base58CodecFlickr();
       const encodedDataWithZero = "11113bYZiKqGF";
       const encoded =
           '2jg5NAth7d2g2QkoJUJvwCcC1fj6GDsCEcJuM1mWo3UMuRWBNsZ3pYXTbD8dPXEB5aEME6uk2A4aD9qvhei957viPswswjBrYfyA17akvt1bBbSnV2cMBQL5QszCZkBRYDETEJTfyEi14ejjE7tuC5G7twfrQPLb144W2eYA9FWVG7LNEyPB7NrpV4yrSN9Td1589eJkeqsDApNfTT8HSFjzRw4hgqvtPpcqKaovUHnRxDu4weLe4DZ4E2Lvn1VFgoBw8LjL7qwJxMp1zgf3NyVjRpp1YreQ4A5EkVpXBgZcP9XSuCEFj3Pwc1qpoE8MYGHK8ieMgCwu999WJzJ7FFRBeTLKyh41YMPVrxKdDBk8Mnms';
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base58FlickrEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base58FlickrDecode(encoded), utf8.encode(testString));
       });
       test('encode with zero', () {
         expect(
-          codec.encode(Uint8List.fromList(dataWithZero)),
+          base58FlickrEncode(Uint8List.fromList(dataWithZero)),
           equals(encodedDataWithZero),
         );
       });
       test('decode with zero', () {
-        expect(codec.decode(encodedDataWithZero), dataWithZero);
+        expect(base58FlickrDecode(encodedDataWithZero), dataWithZero);
       });
     });
     group('Ripple', () {
@@ -245,21 +239,21 @@ void main() {
           'pKGnob7HfDpGpqLPjujWXdUdrEKa6eTdCUjV8rMAPsu8ViAcoTzsQyxtBe3DFxCcnwC8CaVLpbhwe9RWHNJ9nfWJFTXTXKcSyEZbrfwLW7rBcB14vpU8cqmnqT2dzLciyeCtCjtEZCJrhNKKCf7Vdn6f7XESqFmBrhhApNyb9gAv6fmoCZFcfoSQvhZS1o9tDrn39NjLNRTebQoEtt351gK2iXhHGRW7FQURkwPWu54iYeVhXNmNhezhCpmW4rvgGPcX3mKmfRXjY8Qr2GEsoZvKiQQrySNqhbnCLvQxcGzUF9x1VdCgKsFXUrRQPC38y65k3JN8GdXV999Aj2jfggicNtmkZHhry8FvSYkDecL384MT';
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base58RippleEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), utf8.encode(testString));
+        expect(base58RippleDecode(encoded), utf8.encode(testString));
       });
       test('encode with zero', () {
         expect(
-          codec.encode(Uint8List.fromList(dataWithZero)),
+          base58RippleEncode(Uint8List.fromList(dataWithZero)),
           equals(encodedDataWithZero),
         );
       });
       test('decode with zero', () {
-        expect(codec.decode(encodedDataWithZero), dataWithZero);
+        expect(base58RippleDecode(encodedDataWithZero), dataWithZero);
       });
     });
     group('Custom', () {
@@ -549,7 +543,6 @@ void main() {
   });
   group('base85', () {
     group('Ascii', () {
-      const codec = Base85CodecAscii();
       const encoded =
           "<~9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp\$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d\$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4\$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c~>";
       const encodedWithSymbols =
@@ -557,19 +550,18 @@ void main() {
           ";DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@F      D,*)+C] U=@3BN#EcYf 8ATD3s@q?d\$AftVqCh           [NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4\$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c~>";
       test('encode', () {
         expect(
-          codec.encode(Uint8List.fromList(utf8.encode(testString))),
+          base85AsciiEncode(Uint8List.fromList(utf8.encode(testString))),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(utf8.decode(codec.decode(encoded)), testString);
+        expect(utf8.decode(base85AsciiDecode(encoded)), testString);
       });
       test('decode with spec symbols', () {
-        expect(utf8.decode(codec.decode(encodedWithSymbols)), testString);
+        expect(utf8.decode(base85AsciiDecode(encodedWithSymbols)), testString);
       });
     });
     group('Z85', () {
-      const codec = Base85CodecZ();
       const encoded =
           "o<}]Zx(+zcx(!xgzFa9aB7/b}efF?GBrCHty<vdjC{3^mB0bHmvrlv8efFzABrC4raARphB0bKrzFa9dvr9GfvrlH7z/cXfA=k!qz//V7AV!!dx(do{B1wCTxLy%&azC)tvixxeB95Kyw/#hewGU&7zE+pvBzb98ayYQsvixJ2A=U/nwPzi%v}u^3w/\$R}y?WJ}BrCpnaARpday/tcBzkSnwN(](zE:(7zE^r<vrui@vpB4:azkn6wPzj3x(v(iz!pbczF%-nwN]B+efFIGv}xjZB0bNrwGV5cz/P}xC4Ct#zdNP{wGU]6ayPekay!&2zEEu7Abo8]B9hIme=U>K";
       const encodedWithSymbols =
@@ -579,44 +571,43 @@ void main() {
       const rfcTestDataEncoded = "HelloWorld";
       test('encode', () {
         expect(
-          codec.encode(
+          base85ZEncode(
             Uint8List.fromList([...utf8.encode(testString), 0, 0, 0]),
           ),
           equals(encoded),
         );
         expect(
-          codec.encode(
+          base85ZEncode(
             Uint8List.fromList(rfcTestData),
           ),
           equals(rfcTestDataEncoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), [...utf8.encode(testString), 0, 0, 0]);
-        expect(codec.decode(rfcTestDataEncoded), rfcTestData);
+        expect(base85ZDecode(encoded), [...utf8.encode(testString), 0, 0, 0]);
+        expect(base85ZDecode(rfcTestDataEncoded), rfcTestData);
       });
       test('decode with spec symbols', () {
         expect(
-          codec.decode(encodedWithSymbols),
+          base85ZDecode(encodedWithSymbols),
           [...utf8.encode(testString), 0, 0, 0],
         );
       });
     });
     group('IPv6', () {
-      const codec = Base85CodecIPv6();
       //  1080:0:0:0:8:800:200C:417A from RFC 1924
       const address = '108000000000000000080800200C417A';
       const encoded = "4)+k&C#VzJ4br>0wv%Yp";
       test('encode', () {
         expect(
-          codec.encode(
+          base85IPv6Encode(
             Uint8List.fromList(base16.decode(address)),
           ),
           equals(encoded),
         );
       });
       test('decode', () {
-        expect(codec.decode(encoded), base16.decode(address));
+        expect(base85IPv6Decode(encoded), base16.decode(address));
       });
     });
     group('base58Check', () {

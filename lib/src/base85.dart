01ea1a62904e53d9cf119e4c972a7a4d8d6a0598
Arthur Kirsanov <kirsappsinc@gmail.com>
2021-09-29 21:13:27 +0300

    base85 redesign

--- lib/src/base85.dart
+++ lib/src/base85.dart
@@ -3,7 +3,19 @@ import 'dart:typed_data';
 
 import 'constants.dart';
 
-const powList = [52200625, 614125, 7225, 85, 1];
+/// 85 pow 4
+const pow4 = 52200625;
+
+/// 85 pow 3
+const pow3 = 614125;
+
+/// 85 pow 2
+const pow2 = 7225;
+
+/// 85 pow 1
+const pow1 = 85;
+
+const powList = [pow4, pow3, pow2, pow1, 1];
 
 /// Uint 32 max value
 const maxUint32 = 4294967295;
@@ -38,8 +50,7 @@ class Base85CodecAscii extends Codec<Uint8List, String> {
       const Base85EncoderAscii(_alphabet);
 
   @override
-  Converter<String, Uint8List> get decoder =>
-      const Base85DecoderAscii(decodeBase85Ascii);
+  Converter<String, Uint8List> get decoder => const Base85DecoderAscii();
 }
 
 class Base85EncoderAscii extends Converter<Uint8List, String> {
@@ -65,17 +76,62 @@ class Base85EncoderAscii extends Converter<Uint8List, String> {
 }
 
 class Base85DecoderAscii extends Converter<String, Uint8List> {
-  final List<int> _decodeList;
-  const Base85DecoderAscii(this._decodeList);
+  const Base85DecoderAscii();
 
   @override
   Uint8List convert(String input) {
     input = input.replaceAll(base85RegExp, '');
-    return _processDecodeInput(
-      decodeList: _decodeList,
-      input: input,
-      zeroCompressionEnabled: true,
-    );
+    final length = input.length;
+    final output = <int>[];
+    final codeUnits = Uint8List.fromList(input.codeUnits);
+    int unitCounter = 0;
+    int value = 0;
+    for (int i = 0; i < length; i++,) {
+      if (input[i] == 'z') {
+        if (unitCounter == 0) {
+          output.addAll([0, 0, 0, 0]);
+          continue;
+        } else {
+          throw const FormatException(
+            'Misaligned z in input',
+          );
+        }
+      }
+      value += decodeBase85Ascii[codeUnits[i]] * powList[unitCounter];
+      if (unitCounter == 4) {
+        if (value > maxUint32 || value.isNegative) {
+          throw FormatException(
+            'Value result $value larger than max Uint 32 or negative, invalid data provided',
+          );
+        }
+        output.addAll([
+          value >> 24 & 0xFF,
+          value >> 16 & 0xFF,
+          value >> 8 & 0xFF,
+          value & 0xFF
+        ]);
+        value = 0;
+        unitCounter = 0;
+      } else {
+        unitCounter++;
+      }
+    }
+    int padding = 0;
+    if (unitCounter > 0) {
+      while (unitCounter < 5) {
+        /// 117 - u char
+        value += decodeBase85Ascii[117] * powList[unitCounter];
+        unitCounter++;
+        padding++;
+      }
+      output.addAll([
+        value >> 24 & 0xFF,
+        value >> 16 & 0xFF,
+        value >> 8 & 0xFF,
+        value & 0xFF
+      ]);
+    }
+    return Uint8List.fromList(output.sublist(0, output.length - padding));
   }
 }
 
@@ -88,8 +144,7 @@ class Base85CodecZ extends Codec<Uint8List, String> {
   Converter<Uint8List, String> get encoder => const Base85EncoderZ(_alphabet);
 
   @override
-  Converter<String, Uint8List> get decoder =>
-      const Base85DecoderZ(decodeBase85Z);
+  Converter<String, Uint8List> get decoder => const Base85DecoderZ();
 }
 
 class Base85EncoderZ extends Converter<Uint8List, String> {
@@ -112,8 +167,7 @@ class Base85EncoderZ extends Converter<Uint8List, String> {
 }
 
 class Base85DecoderZ extends Converter<String, Uint8List> {
-  final List<int> _decodeList;
-  const Base85DecoderZ(this._decodeList);
+  const Base85DecoderZ();
 
   @override
   Uint8List convert(String input) {
@@ -122,11 +176,25 @@ class Base85DecoderZ extends Converter<String, Uint8List> {
     if (length % 5 != 0) {
       throw ArgumentError('input string must be bounded to 5 bytes');
     }
-    return _processDecodeInput(
-      decodeList: _decodeList,
-      input: input,
-      zeroCompressionEnabled: false,
-    );
+    final output = Uint8List(length ~/ 5 * 4);
+    final codeUnits = Uint8List.fromList(input.codeUnits);
+    for (int i = 0, offset = 0; i < length; i += 5, offset += 4) {
+      final value = (decodeBase85Z[codeUnits[i]] * pow4) +
+          (decodeBase85Z[codeUnits[i + 1]] * pow3) +
+          (decodeBase85Z[codeUnits[i + 2]] * pow2) +
+          (decodeBase85Z[codeUnits[i + 3]] * pow1) +
+          decodeBase85Z[codeUnits[i + 4]];
+      if (value > maxUint32 || value.isNegative) {
+        throw FormatException(
+          'Value result $value larger than max Uint 32 or negative, invalid data provided',
+        );
+      }
+      output[offset] = value >> 24;
+      output[offset + 1] = value >> 16;
+      output[offset + 2] = value >> 8;
+      output[offset + 3] = value & 0xFF;
+    }
+    return output;
   }
 }
 
@@ -227,62 +295,4 @@ String _processEncodeInput({
     offset += 5;
   }
   return String.fromCharCodes(output.sublist(0, output.length - padding));
-}
-
-Uint8List _processDecodeInput({
-  required List<int> decodeList,
-  required String input,
-  required bool zeroCompressionEnabled,
-}) {
-  final length = input.length;
-  final output = <int>[];
-  final codeUnits = Uint8List.fromList(input.codeUnits);
-  int unitCounter = 0;
-  int value = 0;
-  for (int i = 0; i < length; i++,) {
-    if (zeroCompressionEnabled && input[i] == 'z') {
-      if (unitCounter == 0) {
-        output.addAll([0, 0, 0, 0]);
-        continue;
-      } else {
-        throw const FormatException(
-          'Misaligned z in input',
-        );
-      }
-    }
-    value += decodeList[codeUnits[i]] * powList[unitCounter];
-    if (unitCounter == 4) {
-      if (value > maxUint32 || value.isNegative) {
-        throw FormatException(
-          'Value result $value larger than max Uint 32 or negative, invalid data provided',
-        );
-      }
-      output.addAll([
-        value >> 24 & 0xFF,
-        value >> 16 & 0xFF,
-        value >> 8 & 0xFF,
-        value & 0xFF
-      ]);
-      value = 0;
-      unitCounter = 0;
-    } else {
-      unitCounter++;
-    }
-  }
-  int padding = 0;
-  if (unitCounter > 0) {
-    while (unitCounter < 5) {
-      /// 117 - u char
-      value += decodeList[117] * powList[unitCounter];
-      unitCounter++;
-      padding++;
-    }
-    output.addAll([
-      value >> 24 & 0xFF,
-      value >> 16 & 0xFF,
-      value >> 8 & 0xFF,
-      value & 0xFF
-    ]);
-  }
-  return Uint8List.fromList(output.sublist(0, output.length - padding));
 }
